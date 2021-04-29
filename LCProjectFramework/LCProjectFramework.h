04f659d0d9020f381045132870de89c25e59e86a
lichuang <lichuang02@sunlands.com>
2021-04-29 17:50:48 +0800

    11

---
+++ LCProjectFramework/LCProjectFramework.h
@@ -0,0 +1,18 @@
+//
+//  LCProjectFramework.h
+//  LCProjectFramework
+//
+//  Created by Admin on 2021/4/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LCProjectFramework.
+FOUNDATION_EXPORT double LCProjectFrameworkVersionNumber;
+
+//! Project version string for LCProjectFramework.
+FOUNDATION_EXPORT const unsigned char LCProjectFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LCProjectFramework/PublicHeader.h>
+
+
